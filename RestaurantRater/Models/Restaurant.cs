129316c655944d7907e9b77bf8da1d9681b75ab3
Kyle Hazen <kylehazen127@gmail.com>
2020-02-25 10:44:19 -0500

    Add project files.

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        //This would be our restaurant model
+        //this builds the primary key, which gets us into the data table and allows us to index through it (in it?). Currently the primary key because it is first in the list. Must always have a primary key when dealing with entity framework 
+        
+        [Key] //Making this a key automatically makes it a required field 
+        public int Id { get; set; } 
+
+        [Required] //annotation goes to the first thing it can apply to and then stops 
+        public string Style { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0d, 5d)]
+        public double Rating { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int DollarSigns { get; set; }
+
+    }
+}
---
+++ RestaurantRater/Models/RestaurantDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class RestaurantDbContext : DbContext
+    {
+        //This constructor is connecting to the "connection string" of code --> see "name" in web.config section should be the same as what you put in the base ( )
+
+        public RestaurantDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        //creating a table for our model. Takes in a "T entity" (i.e. entity framework). Typically entity is database level model
+        //Then call in our Restaurant class (i.e. model) 
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
